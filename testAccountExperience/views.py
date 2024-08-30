2ce7d86d29e700d0cdbd10b39c6b6bb53b3eb8cf
Luke Pemberton <pembertonf50@gmail.com>
2024-08-30 16:57:24 +0100

    Created a login + signup base template and simplified and cleaned templates code

--- testAccountExperience/views.py
+++ testAccountExperience/views.py
@@ -1,18 +1,33 @@
 from django.http import HttpResponse
 from django.template import loader
 
-""" Example syntax
-def home(request):
-  context = {"name": "Junior"}
-  template = loader.get_template("app/home.html")
-  return HttpResponse(template.render(context))
-"""
-
 # Create your views here.
-
 def home(request):
   template = loader.get_template("testAccountExperience/homePage.html")
-  context = {"answer": "yes"}
+  accounts = [
+    {"email": "faker1@gmail.com",
+     "password": "123456",
+     "location": "united kingdom",
+     "language": "english",
+     "subscriptions": [],
+     "card": "yes",
+     "address": "yes",
+     "experiences": "http://www.faker1.com"},
+
+    {"email": "faker2@gmail.com",
+     "password": "123456",
+     "location": "france",
+     "language": "french",
+     "subscriptions": ["prime"],
+     "card": "no",
+     "address": "no",
+     "experiences": "http://www.faker2.com"}
+  ]
+  userInfo = {"loggedIn": True, "email": "randomEmail@outlook.com", "password": "<PASSWORD>" }
+  context = {"accounts": accounts, "userInfo": userInfo}
   return HttpResponse(template.render(context))
 
-
+def logIn(request):
+  template = loader.get_template("testAccountExperience/signUp.html")
+  context = {"somehting": "somehting"}
+  return HttpResponse(template.render(context))
