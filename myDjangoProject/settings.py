71bb505466c536fac818dcbacc6951d27a37d229
Luke Pemberton <pembertonf50@gmail.com>
2024-10-02 16:05:57 +0100

    Adding whitenoise to requirements so that the admin has css and styling in production

--- myDjangoProject/settings.py
+++ myDjangoProject/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # Todo: whitenoise allows css and other static files to be available in prod admin endpoint
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +121,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Todo: This setting informs Django of the URI path from which your static files will be served to users
+#  Here, they well be accessible at your-domain.onrender.com/static/...
+STATIC_URL = '/static/'
+
+# Todo: This production code might break development mode, so we check whether we're in RENDER_PROD
+if os.environ.get('RENDER_PROD') == 'True':
+    # Todo: Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Todo: Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    #   and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
