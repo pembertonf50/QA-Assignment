cc77a3e7f657c3cbddc5ae050ffe65950d4e6e44
Luke Pemberton <pembertonf50@gmail.com>
2025-06-09 20:39:55 +0100

    Added logs to the view functions

--- testAccountExperience/views.py
+++ testAccountExperience/views.py
@@ -8,8 +8,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from faker import Faker
 from .models import TestAccount
-from .appCupboard import stringifySubscriptions, listifySubscriptions, subscriptions, locations, languages, \
-  dummyAccounts, generateUniqueEmail, country_iso_codes, urlPlanType
+from .appCupboard import *
 
 
 # Create your views here.
@@ -21,8 +20,8 @@ def home(request):
     testAccountLocation = request.POST['location']
     testAccountLanguage = request.POST['language']
     testAccountSubscriptions = request.POST.getlist('subscriptions')
-    testAccountCardSaved = request.POST['card'] == "yes"
-    testAccountAddressSaved = request.POST['address'] == "yes"
+    testAccountCardSaved = request.POST['card'] == 'yes'
+    testAccountAddressSaved = request.POST['address'] == 'yes'
 
     # Todo: This supplies middle-end validation
     if country_iso_codes.get(testAccountLocation) and testAccountLanguage in languages:
@@ -36,12 +35,18 @@ def home(request):
         subscriptions=stringifySubscriptions(testAccountSubscriptions),  # Todo: appCupboard.py has documentation
         cardSaved=testAccountCardSaved,
         addressSaved=testAccountAddressSaved,
-        experienceLink=f"www.virtualmachine-{planType}{country_iso_codes[testAccountLocation].lower()}.com",
+        experienceLink=f'www.virtualmachine-{planType}{country_iso_codes[testAccountLocation].lower()}.com',
         testAccountOwner=user
       ).save()
 
     else:
-      messages.error(request, "Need to select a value from Account location and Account language")
+      messages.error(request, 'Need to select a value from Account location and Account language')
+      verboseLog(message='failed to create test account.',
+                 ipAddress=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR'),
+                 userEmail=user.email,
+                 testAccountLocation=testAccountLocation, testAccountLanguage=testAccountLanguage,
+                 testAccountSubscriptions=testAccountSubscriptions, testAccountCardSaved=testAccountCardSaved,
+                 testAccountAddressSaved=testAccountAddressSaved)
 
     # Todo: the use of redirect stops multiple POST submission which duplicates account creation on refresh
     return redirect('home')
@@ -51,26 +56,30 @@ def home(request):
     testAccounts = TestAccount.objects.filter(testAccountOwner=user)
     if testAccounts:
       accounts = listifySubscriptions(testAccounts)  # Todo: appCupboard.py has documentation
-    context = {"accounts": accounts, "subscriptions": subscriptions, "locations": locations, "languages": languages,
-               "userEmail": userEmail, "loggedIn": user.is_authenticated}
-    return render(request, "testAccountExperience/homePage.html", context)
-  context = {"accounts": accounts, "subscriptions": subscriptions, "locations": locations, "languages": languages}
-  return render(request, "testAccountExperience/homePage.html", context)
+    context = {'accounts': accounts, 'subscriptions': subscriptions, 'locations': locations, 'languages': languages,
+               'userEmail': userEmail, 'loggedIn': user.is_authenticated}
+    return render(request, 'testAccountExperience/homePage.html', context)
+  context = {'accounts': accounts, 'subscriptions': subscriptions, 'locations': locations, 'languages': languages}
+  return render(request, 'testAccountExperience/homePage.html', context)
   # Todo: render() displays the chosen template such as homePage.html to the user. context is used to pass data to
   #   template
 
 def logIn(request):
   if request.method == 'POST':
-    email = request.POST["email"]
-    password = request.POST["password"]
+    email = request.POST['email']
+    password = request.POST['password']
     user = authenticate(request, username=email, password=password)
     # Todo: Check if a user is verified and authenticated
     if user is not None:
       login(request, user)
-      return redirect("home")
+      return redirect('home')
     else:
-      messages.error(request, "Cannot find account with these login credentials")
-  return render(request, "testAccountExperience/logIn.html")
+      messages.error(request, 'Cannot find account with these login credentials')
+      verboseLog(message='failed to switch account.' if request.user.is_authenticated else 'failed to login.',
+                 ipAddress=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR'),
+                 userEmail=request.user.email if request.user.is_authenticated else '(user not signed in)',
+                 emailInput=email, passwordInput=password)
+  return render(request, 'testAccountExperience/logIn.html')
 
 def signUp(request):
   if request.method == 'POST':
@@ -81,45 +90,57 @@ def signUp(request):
     #   has valid email and passwords
     if User.objects.filter(email=email).exists():
         valid = False
-        messages.error(request, "Email already registered")
+        messages.error(request, 'Email already registered')
+        verboseLog(message='email already registered.',
+                   ipAddress=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR'),
+                   userEmail=request.user.email if request.user.is_authenticated else '(user not signed in)',
+                   emailInput=email, passwordInput=password)
     try:
       validate_email(email)
     except ValidationError as e:
       valid = False
-      messageString = "Bad email, details: " + str(e)
+      messageString = 'Bad email, details: ' + str(e)
       messages.error(request, messageString)
+      verboseLog(message='invalid email used.',
+                 ipAddress=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR'),
+                 userEmail=request.user.email if request.user.is_authenticated else '(user not signed in)',
+                 emailInput=email, passwordInput=password)
     try:
       # Todo: validate_password() checks common passwords, similar to user properties, not completely numeric and
       #  minimum 9 characters long
       validate_password(password)
     except ValidationError as e:
       valid = False
-      messageString = "Bad password, details: " + str(e)
+      messageString = 'Bad password, details: ' + str(e)
       messages.error(request, messageString)
+      verboseLog(message='insufficiently strong password.',
+                 ipAddress=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR'),
+                 userEmail=request.user.email if request.user.is_authenticated else '(user not signed in)',
+                 emailInput=email, passwordInput=password)
 
     if valid:
       User.objects.create_user(username=email, email=email, password=password).save()
       userAuthenticated = authenticate(request, username=email, password=password)
       if userAuthenticated is not None:
         login(request, userAuthenticated)
-        return redirect("home")
+        return redirect('home')
       else:
-        return redirect("login")
-  return render(request, "testAccountExperience/signUp.html")
+        return redirect('login')
+  return render(request, 'testAccountExperience/signUp.html')
 
 def deleteAccount(request):
     if not request.user.is_authenticated:
-      return redirect("home")
-    if request.user.is_authenticated and request.method == "POST":
+      return redirect('home')
+    if request.user.is_authenticated and request.method == 'POST':
         User.objects.filter(email=request.user.email).delete()
-        return redirect("home")
-    context = {"userEmail": request.user.email}
-    return render(request, "testAccountExperience/deleteAccount.html", context)
+        return redirect('home')
+    context = {'userEmail': request.user.email}
+    return render(request, 'testAccountExperience/deleteAccount.html', context)
 
 def logOut(request):
   if request.user.is_authenticated:
     logout(request)
-  return redirect("home")
+  return redirect('home')
 
 def deleteTestAccount(request, templateEmail):
   if request.user.is_authenticated:
@@ -127,4 +148,4 @@ def deleteTestAccount(request, templateEmail):
     testAccountToDelete = TestAccount.objects.filter(Q(email=templateEmail) & Q(testAccountOwner=request.user))
     if testAccountToDelete.exists():
       testAccountToDelete.delete()
-  return redirect("home")
+  return redirect('home')
